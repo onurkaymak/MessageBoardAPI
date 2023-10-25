b5e73ac1e5436a4cd63a24e69643410e37454ee4
onurkaymak <onurkaymak@outlook.com>
2023-10-25 14:53:51 -0700

    Add SignIn and Register class as data transfer objects.

---
+++ MessageBoardApi/Models/SignInDto.cs
@@ -0,0 +1,8 @@
+namespace MessageBoardApi.Models;
+
+public class SignInDto
+{
+  public string Email { get; set; }
+  public string UserName { get; set; }
+  public string Password { get; set; }
+}
